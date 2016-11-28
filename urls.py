063b78a9ade426a187ca3d011aae58a081f2995e
horiebin <horiebin@foxmail.com>
2016-11-28 16:00:06 +0100

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,31 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from polls import views
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    # ex: /polls/index
+    url(r'^index$', views.index, name='index'),
+    # ex: /detail
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: polls/2/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', \
+    views.results, name='results'),
+    #ex:/polls/2/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', \
+    views.vote, name='vote'),
+]
